ab158052919b56005df82eb9b79c00e57c735beb
srsm2000 <srsm2000.abv@gmail.com>
2020-10-21 10:22:06 +0900

    出力のコードを、素数判断のコードから分割

--- 02/if_5.php
+++ 02/if_5.php
@@ -2,14 +2,20 @@
 
 $num = 10;
 
+// 素数かどうかをチェックする
 for ($i = 2; $i < $num; $i++) {
     if ($num % $i == 0) {
     $res = $i;
-    echo $num . 'は素数ではありません。';
     break;
     }
 }
 
-if($res == NULL) {
-    echo $num . "は素数です。";
+// 出力
+$prime_number = $num . 'は素数です。';
+$not_prime_number = $num . 'は素数ではありません。';
+
+if($res) {
+    echo $not_prime_number;
+} else {
+    echo $prime_number;
 }
