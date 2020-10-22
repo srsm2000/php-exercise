d787368332f188da80c549fad87ad450c23adabc
srsm2000 <srsm2000.abv@gmail.com>
2020-10-22 13:38:38 +0900

    課題_オブジェクト指向2

--- 06/index.php
+++ 06/index.php
@@ -3,9 +3,9 @@
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
 class Car
 {
-    private $name;
-    private $number;
-    private $color;
+    protected $name;
+    protected $number;
+    protected $color;
 
     // コンストラクタ
     public function __construct($name, $number, $color)
@@ -51,42 +51,55 @@ class Car
     }
 }
 
-// 以下を実行した時にエラーがでないようにして下さい。
+// 以下にCarクラスを継承したTaxiクラスを記述して下さい
+class Taxi extends Car
+{
+    private $passenger = 0;
+
+    public function pickUp($get_on)
+    {
+        $this->passenger = $this->passenger + $get_on;
+    }
+
+    public function lower($get_off)
+    {
+        if ( $this->passenger - $get_off >= 0) {
+            $this->passenger = $this->passenger - $get_off;
+            echo $get_off . "人降車しました。";
+        } else {
+            echo "降車人数に誤りがあります";
+        }
+    }
+
+    public function infomation()
+    {
+        echo '車の車種:' . $this->name . '、車体番号:' . $this->number . '、カラー:' . $this->color . '、乗車人数は' . $this->passenger . '人です。';
+    }
+}
+
+// 以下を実行した時にエラーがでないようにして下さい
 // Carクラスのインスタンスを生成
 // 引数 : 車名、車体番号、カラー
 $car1 = new Car('ランクル', 100, 'black');
 
-// 車名(車種)を取得
-echo $car1->getName();
-echo '<br>';
-
-// 車体番号を取得
-echo $car1->getNumber();
-echo '<br>';
-
-// カラーを取得
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
+// 車の情報を表示
 echo $car1->infomation();
 echo '<br>';
+echo '<hr>';
 
-// 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
+// Taxiクラスのインスタンスを生成
+$taxi1 = new Taxi('クラウンのタクシー', 222, 'black');
+
+// 3人乗車
+$taxi1->pickUp(3);
+
+// タクシーの情報表示を表示
+echo $taxi1->infomation();
 echo '<br>';
 
-// 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
+// 2人降車
+$taxi1->lower(2);
 echo '<br>';
 
-// カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo '<br>';
-
-// 車の情報表示を表示
-echo $car1->infomation();
-echo '<br>';
+// 一人降車
+$taxi1->lower(2);
